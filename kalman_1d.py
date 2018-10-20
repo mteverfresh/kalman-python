744b19dbf55843c5a1e920d05da9e6a206b08e38
mteverfresh <shererzach@gmail.com>
2018-10-20 13:51:50 -0400

    Added first project file.

---
+++ kalman_1d.py
@@ -0,0 +1,56 @@
+#  Filename: kalman_1d.py
+#  Author: Zach Sherer
+#  Purpose: Implements a 1d kalman filter on scalar values for a single input parameter.
+#  Notes:
+#    some definitions:
+#     gain: kalman gain vector for all measurements
+#     estimate_error: error in all estimates. Consider this the process noise.
+#     estimate: all estimates. holds the current estimate outside of functions, but will act as 
+#                   the previous estimate when used as a function arg. This will make sense in the code.
+#     measurement_error: error in all measurements. Consider this the measurement noise.
+#     measurement: holds all measurements. Has the same properties as estimate.
+
+import numpy as np
+import matplotlib as mp
+
+def kalman_gain(estimate_error, measurement_error):
+    return estimate_error/(estimate_error * measurement_error)
+
+def new_est(estimate, measurement, estimate_error, measurement_error):
+    kg = kalman_gain(estimate_error, measurement_error)
+    return estimate + kg*(measurement - estimate)
+
+def new_error(estimate_error, measurement_error):
+    kg = kalman_gain(estimate_error, measurement_error)
+    return (1-kg)*estimate_error
+
+def kalman_filter:
+    #initialize all parameters
+    estimate = 27
+    estimate_error = 1
+
+    true_value = 23
+    measurement_error = 1.5
+    measurements = np.random.normal(true_value, measurement_error, 100) #draw measurements from a gaussian distribution with some error
+
+    #keep track of estimates and error
+    all_estimates = np.zeroes(100, dtype=float)
+    all_est_error = np.zeroes(100, dtype=float)
+
+    for i in range(100):
+        #Kalman gain is calculated as part of new estimate and new error calculations
+        estimate = new_est(estimate, measurement[i], estimate_error, measurement_error)
+        estimate_error = new_error(estimate_error, measurement_error)
+
+        #update estimates and error for later plotting
+        all_estimates[i] = estimate
+        all_est_error[i] = estimate_error
+
+        print(estimate, estimate_error, true value)
+
+    
+        
+kalman_filter();
+
+
+
