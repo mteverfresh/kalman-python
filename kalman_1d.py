8c5e03d328c124bc59fab389dfbd7e8dcfcc514d
mteverfresh <shererzach@gmail.com>
2018-10-21 14:33:57 -0400

    1D filter complete. Plot generated with error and estimate.

--- kalman_1d.py
+++ kalman_1d.py
@@ -11,37 +11,38 @@
 #     measurement: holds all measurements. Has the same properties as estimate.
 
 import numpy as np
-import matplotlib as mp
+import matplotlib.pyplot as mat
 
 def kalman_gain(estimate_error, measurement_error):
-    return estimate_error/(estimate_error * measurement_error)
+    return estimate_error/(estimate_error + measurement_error)
 
-def new_est(estimate, measurement, estimate_error, measurement_error):
-    kg = kalman_gain(estimate_error, measurement_error)
+def new_est(estimate, measurement, estimate_error, measurement_error, kg):
     return estimate + kg*(measurement - estimate)
 
-def new_error(estimate_error, measurement_error):
-    kg = kalman_gain(estimate_error, measurement_error)
+def new_error(estimate_error, measurement_error, kg):
     return (1-kg)*estimate_error
 
 def kalman_filter():
     #initialize all parameters
     estimate = 27.0
     estimate_error = 1.5
+    iterations = input("Enter the number of iterations: ")
 
     true_value = 20
     measurement_error = 0.5
-    measurements = np.random.normal(true_value, measurement_error, 100) #draw measurements from a gaussian distribution with some error
+    measurements = np.random.normal(true_value, measurement_error, iterations) #draw measurements from a gaussian distribution with some error
 
     #keep track of estimates and error
-    all_estimates = np.zeros(100, dtype=float)
-    all_est_error = np.zeros(100, dtype=float)
+    all_estimates = np.zeros(iterations, dtype=float)
+    all_est_error = np.zeros(iterations, dtype=float)
 
-    for i in range(100):
+    print(measurements)
+
+    for i in range(iterations):
         #Kalman gain is calculated as part of new estimate and new error calculations
         kg = kalman_gain(estimate_error, measurement_error)
-        estimate = new_est(estimate, measurements[i], estimate_error, measurement_error)
-        estimate_error = new_error(estimate_error, measurement_error)
+        estimate = new_est(estimate, measurements[i], estimate_error, measurement_error, kg)
+        estimate_error = new_error(estimate_error, measurement_error, kg)
 
         #update estimates and error for later plotting
         all_estimates[i] = estimate
@@ -49,6 +50,26 @@ def kalman_filter():
 
         print("Estimate: ", estimate, "\tMeasurement: ", measurements[i])
         print("Estimate error: ", estimate_error)
-        print("Kalman gain: ", kg)
+        print(kg)
+
+    #get ready to plot
+    x_axis = range(iterations)
+    true_value_plot = [true_value for i in range(iterations)]
+    
+    fig, axis = mat.subplots(2, 1)
+
+    axis[0].plot(x_axis, all_estimates, label="estimates")
+    axis[0].plot(x_axis, measurements, label="measurements")
+    axis[0].plot(x_axis, true_value_plot, label="true value")
+    axis[0].set_ylabel("Temperature")
+    axis[0].set_xlabel("Sample Number")
+    axis[0].legend(loc="upper right", frameon=False)
+
+    axis[1].plot(x_axis, all_est_error, label="error")
+    axis[1].set_ylabel("Error")
+    axis[1].set_xlabel("Sample Number")
+
+    fig.tight_layout()
+    mat.show()
         
 kalman_filter();
